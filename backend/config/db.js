c675eb1f7fbc651c516a59c8f940f3cbf429625e
Balamanikandan <balamanikandanb191@github.com>
2026-06-24 23:25:08 +0530

    Fix DB connection pool

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,22 +1,28 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-connection.connect((err) => {
+const connection = pool;
+
+pool.getConnection((err, conn) => {
   if (err) {
-    console.error(err);
+    console.error("Database Connection Failed:", err);
   } else {
     console.log("DB Connected Successfully");
     // Run DB Migrations
     const runMigration = require("./migrate");
     runMigration();
+    conn.release();
   }
 });
 
